11bf1a3a32be50bd9dfe608b20e3d60d8e898737
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-04-07 08:36:33 +0530

    message displaying added

--- routes/login.js
+++ routes/login.js
@@ -2,27 +2,27 @@ const router = require("express").Router();
 const userModel = require("../models/user");
 
 router.get("/", (req, res) => {
-	// making a empty login credential object
-	let loginCredential = new userModel({
-		username: "",
-		password: "",
+	res.render("login/login", {
+		data: {
+			message: "",
+			username: "",
+			password: "",
+		},
 	});
-	res.render("login/login", { loginCredential });
 });
 
 router.post("/", async (req, res) => {
-	let loginCredential = new userModel({
-		username: req.body.username,
-		password: req.body.password,
-		email: req.body.email,
-	});
 	const user = await userModel.findOne({ username: req.body.username });
-	if (user.password === req.body.password) {
+	if (user && user.password === req.body.password) {
 		res.send("Login Complete User authenticated");
 	} else {
 		// if credentials are not correct the username will stay same and the password will be empty
 		res.render("login/login", {
-			loginCredential: { ...loginCredential, password: " " },
+			data: {
+				username: req.body.username,
+				password: "",
+				message: "Incorrect Credentials",
+			},
 		});
 	}
 });
