3b8f1c9c9d6b57190d915454751d3b34fbcf0979
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-04-07 07:39:03 +0530

    post method to save user

--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,19 @@ router.get("/", (req, res) => {
 	res.render("register/register");
 });
 
+router.post("/", async (req, res) => {
+	const { username, password, email } = req.body;
+	const user = new userModel({ username, password, email });
+	await user.save((err, user) => {
+		if (err) {
+			res.render("register/register", {
+				username,
+				email,
+			});
+		} else {
+			res.redirect("/");
+		}
+	});
+});
+
 module.exports = router;
