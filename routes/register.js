bb293d37bc529b521e32ebf4c9065ad923d49756
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-04-07 08:03:13 +0530

    post method to save user updated

--- routes/register.js
+++ routes/register.js
@@ -6,18 +6,20 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-	const { username, password, email } = req.body;
-	const user = new userModel({ username, password, email });
-	await user.save((err, user) => {
-		if (err) {
-			res.render("register/register", {
-				username,
-				email,
-			});
-		} else {
-			res.redirect("/");
-		}
+	let user = await userModel.find({
+		username: req.body.username,
 	});
+	if (user) {
+		res.render("register/exists");
+	} else {
+		const newUser = new userModel({
+			username: req.body.username,
+			password: req.body.password,
+			email: req.body.email,
+		});
+		newUser.save();
+		res.render("register/success");
+	}
 });
 
 module.exports = router;
